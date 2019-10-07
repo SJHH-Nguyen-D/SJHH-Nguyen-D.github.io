6802662d77339d0c57949673925a54fb14511c22
Dennis Nguyen-Do <dennisnguyendo@gmail.com>
2019-10-07 17:47:31 -0400

    setting up website

---
+++ Pelican/content/blog/test.rst
@@ -0,0 +1,127 @@
+Testing Testing 1, 2, 3
+#######################
+
+:date: 20180928 10:30
+:tags: tutorial
+:category: test
+:slug: test
+:author: Nguyen-Do, Dennis
+:summary: Does this even work
+
+.. todo:
+  0. Get the content and writing skeleton down first
+  2. Social Media Links
+  4. Output Plots for EDA
+  7. Deploy it to github pages
+  8. get user input from html forms and calculate a job performance score....Do i really need to use flask for this?
+  9. after you spin up all the html files with pelican. stop using pelican and serve with flask and dash
+
+This is about post about testing testing whether this thing even works
+
+.. image:: https://live.staticflickr.com/4364/35630875744_5cff0b53b6_c_d.jpg
+    :height: 427px
+    :width: 640px
+    :alt: moss on a log
+    :align: center
+    :name: my picture
+
+*Moss on a Log* by `dvpho_tos <https://www.flickr.com/photos/dvpho_tos/35630875744/>`_
+
+`` for i in range(0, 4): print('Hello World!)``
+
+
+`python <www.google.com>`_ as a link
+
+*python* in Italics
+
+**python** bolded
+
+This is an ordinary paragraph, introducing a block quote.
+
+    "It is my business to know things.  That is my trade."
+
+    -- Sherlock Holmes
+
+what
+  Definition lists associate a term with a definition.
+
+*how*
+  The term is a one-line phrase, and the definition is one or more
+  paragraphs or body elements, indented relative to the term.
+  Blank lines are not allowed between term and definition.
+
+**when**
+  The term is a one-line phrase, and the definition is one or more
+  paragraphs or body elements, indented relative to the term.
+  Blank lines are not allowed between term and definition.
+
+.. code-block:: python
+
+  import numpy as np
+  import scipy.stats as stats
+  import pylab as pl
+
+  for country in df['cntryid_e'].unique()[pd.Series(df['cntryid_e'].unique()).isnull() == False]:
+      
+      country_grouped_df = df[df['cntryid_e'] == country]
+      
+      h = sorted(country_grouped_df['job_performance'].values)
+
+      fit = stats.norm.pdf(h, np.mean(h), np.std(h))  #this is a fitting indeed
+
+      pl.plot(h,fit,'--')
+
+      pl.hist(h,normed=True)  #use this to draw histogram of your data
+      
+      pl.title(f"Distribution of Job Performance Scores by {country}")
+                
+      pl.show()     
+
+The regular expression ``[+-]?(\d+(\.\d*)?|\.\d+)`` matches
+floating-point numbers (without exponents).
+
+:code:`a = b + c`
+
+.. role: python(code)
+  :language: python
+
+
+  import matplotlib.pyplot as plt 
+  import seaborn as sns 
+  import pandas as pd 
+  from sklearn.datasets import *
+
+  iris = load_iris()
+  df = pd.DataFrame(iris.data, columns=iris.feature_names)
+  df.target = iris.target
+
+How to publish a User Page with GitHub Pages::
+
+$ pelican content -o output -s pelicanconf.py
+$ ghp-import output
+$ git push git@github.com:SJHH-Nguyen-D/SJHH-Nguyen-D.github.io.git master:master
+
+.. image:: https://live.staticflickr.com/4345/36465727015_3a918829bc_k_d.jpg
+    :height: 427px
+    :width: 640px
+    :alt: moss on a log
+    :align: center
+    :name: log
+
+Using the container directive:
+
+.. container:: custom
+
+   This paragraph might be rendered in a custom way. In a container.
+
+
+Using the csv-table directive which can be done inline or with an external file. This can introduce security holes potentially:
+
+.. csv-table:: Frozen Delights!
+   :header: "Treat", "Quantity", "Description"
+   :widths: 15, 10, 30
+
+   "Albatross", 2.99, "On a stick!"
+   "Crunchy Frog", 1.49, "If we took the bones out, it wouldn't be
+   crunchy, now would it?"
+   "Gannet Ripple", 1.99, "On a stick!"
---
+++ Pelican/pelicanconf.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'Dennis Nguyen-Do'
+SITENAME = 'Denny-4/7 Data Science Blog'
+SITEURL = 'https://sjhh-nguyen-d.github.io'
+SITE_DESCRIPTION = u'My name is Dennis Nguyen-Do. This is my personal blog.'
+
+PATH = 'content'
+
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+# PAGE_PATHS = ['pages']
+# ARTICLE_PATHS = ['blog']
+
+# STATIC PATHS needs to be set to allow us to reference our photos with: /assets/image.png
+# This folder is also just a basename, and is relative to the PATH specified so: content/assets
+# static paths will be copied without parsing their contents
+# remember, don't be using slashes in the string for the assets folder...things do not load this way
+# however, when you make reference to them in the reference line, you can add the slash, but you don't put the path into quotations
+STATIC_PATHS = ['assets']
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'categories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'en'
+
+# If content doesn't reload upon update, set this option to False
+LOAD_CONTENT_CACHE = False
+CACHE_CONTENT = False
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),)
+
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/Dennis00481552'),
+          ('github', 'https://github.com/SJHH-Nguyen-D/'),)
+
+DEFAULT_PAGINATION = 5
+DEFAULT_ORPHANS = 1
+PAGINATED_TEMPLATES = {'index': None, 'tag': None, 'category': None, 'author': None}
+
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+SUMMARY_MAX_LENGTH = None
+
+HEADER_COVER = "/assets/head_cover_general.jpg"
+
+HEADER_COVERS_BY_TAG = {
+  'articles_cover': 'assets/head_cover_articles.jpg', 
+  'pages_cover': '/assets/head_cover_pages.jpg',
+  'general_cover': '/assets/head_cover_general.jpg',
+}
+
+# Pelican Themes
+THEME = "attila" # installed with pelican-themes --install /path/to/attila/theme/from/githubrepo
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+AUTHORS_BIO = {
+  "Dennis Nguyen-Do": {
+    "name": "Dennis Nguyen-Do",
+    "cover": "unavailable",
+    "image": "unavailable",
+    "website": "unavailable",
+    "linkedin": "https://www.linkedin.com/in/dennisnguyendo/",
+    "github": "SJHH-Nguyen-D",
+    "location": "Toronto, ON",
+    "bio": "This is the place for a small biography with max 200 characters. Well, now 100 are left. Cool, hugh?"
+  }
+}
+
+
+# If you want to disable creation of html files that use tags, uncomment the two lines below
+# TAGS_SAVE_AS = ''
+# TAG_SAVE_AS = ''
+
+# if you only want to build out a select few pages at a time instead of creating everything from scratch, put them in this list
+# If you want to build up everything from scratch because you're not sure which files you made changes in already, you can uncomment the line below
+# WRITE_SELECTED = ['/2019/10/sharpestminds-project-part-2.html', '/2018/09/test.html']
+
+# ************ TEMPLATES **********************
+# If you want to generate custom pages besides your blog entries, 
+# you can point any Jinja2 template file with a path pointing to
+# the file and the destination path for the generated file.
+# TEMPLATE_PAGES = {'templates/books.html': 'output/pages/books.html',
+#                   'templates/resume.html': 'output/pages/resume.html',
+#                   'templates/contact.html': 'output/pages/contact.html'}
+
+
+DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives']
+
+THEME_TEMPLATES = ['/theme/templates/']
